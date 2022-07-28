adf8a8e044a91580fa98fbca8a4a7498cf75272d
Elven Lin, RetrO9 <elvenlin1998@gmail.com>
2022-07-28 17:31:17 +0800

    go: 98 Validate Binary Search Tree

---
+++ Problems/98/main.go
@@ -0,0 +1,41 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func main() {
+
+}
+
+func isValidBST(root *TreeNode) bool {
+	// Unique Node
+	// Preorder traversal to check subtree
+
+	if root == nil {
+		return true
+	}
+
+	result, _, _ := vTree(root)
+	return result
+}
+
+func vTree(root *TreeNode) (bool, int, int) {
+	// isOk, min, max
+	// root is not allowed nil
+	result, ok := true, true
+	lmin, lmax, rmin, rmax := root.Val, root.Val, root.Val, root.Val
+	if root.Left != nil {
+		ok, lmin, lmax = vTree(root.Left)
+		result = result && ok && lmax < root.Val
+	}
+
+	if result && root.Right != nil {
+		ok, rmin, rmax = vTree(root.Right)
+		result = result && ok && rmin > root.Val
+	}
+
+	return result, lmin, rmax
+}
---
+++ Problems/98/readme.md
@@ -0,0 +1,5 @@
+# Validate Binary Search Tree
+
+| Solution Idx | Time Complexity | Space Complexity | Comment          |
+| ------------ | --------------- | ---------------- | ---------------- |
+| 1            | O(n)            | O(1)             | Typical Problems |
