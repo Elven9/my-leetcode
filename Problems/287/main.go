77928f039aba0000f884fdc04180ec4dd96de539
Elven Lin, RetrO9 <elvenlin1998@gmail.com>
2022-10-25 09:22:36 +0800

    go: 287 update

---
+++ Problems/287/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "sort"
+
+func main() {}
+
+func findDuplicate(nums []int) int {
+	idx := sort.Search(len(nums), func(i int) bool {
+		mcnt, cnt := 0, 0
+		i++
+
+		for _, n := range nums {
+			if n == i {
+				mcnt++
+			} else if n < i {
+				cnt++
+			}
+		}
+
+		if mcnt > 1 {
+			return true
+		}
+
+		if cnt <= i-1 {
+			return false
+		} else {
+			return true
+		}
+	})
+
+	if idx != len(nums) {
+		return idx + 1
+	}
+	return 0
+}
