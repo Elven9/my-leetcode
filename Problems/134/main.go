a74c06bdce21dd75f786de4e096cf990c91f1354
Elven Lin, RetrO9 <elvenlin1998@gmail.com>
2023-10-20 09:13:54 +0800

    feat: update 134

--- Problems/134/main.go
+++ Problems/134/main.go
@@ -4,6 +4,51 @@ func main() {}
 
 func canCompleteCircuit(gas []int, cost []int) int {
 	// Faster version with O(n) greedy
+	m, tg, tc := len(gas), 0, 0
+
+	for i := 0; i < m; i++ {
+		tg += gas[i]
+		tc += cost[i]
+	}
+
+	if tg < tc {
+		return -1
+	}
+
+	for i := 0; i < m; i++ {
+		cur := i
+		ga := gas[cur] - cost[cur]
+		var skip int
+
+		if ga < 0 {
+			continue
+		}
+		cur++
+		if cur == m {
+			cur = 0
+		}
+
+		for {
+			ga += gas[cur] - cost[cur]
+			if ga >= 0 && cur != i {
+				if cur+1 == m {
+					cur = 0
+				} else {
+					cur++
+				}
+			} else {
+				skip = cur
+				break
+			}
+		}
+
+		if cur == i && ga >= 0 {
+			return i
+		}
+		i = skip
+	}
+
+	return -1
 }
 
 func canCompleteCircuit_slow(gas []int, cost []int) int {
--- Problems/134/readme.md
+++ Problems/134/readme.md
@@ -3,3 +3,4 @@
 | Solution Idx | Time Complexity | Space Complexity | Comment                                  |
 | ------------ | --------------- | ---------------- | ---------------------------------------- |
 | 1            | O(n^2)          | O(1)             | Energy solution on every starting points |
+| 2            | O(n)            | O(1)             | Remove redundant check                   |
