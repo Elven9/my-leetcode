c7ab10c5f64731f6e0cf6dd8cf2455ce79d60c0e
Elven Lin, RetrO9 <elvenlin1998@gmail.com>
2024-07-03 10:21:26 +0800

    update: 2597

--- Problems/2597/readme.md
+++ Problems/2597/readme.md
@@ -3,3 +3,12 @@
 | Solution Idx | Time Complexity | Space Complexity | Comment                                               |
 | ------------ | --------------- | ---------------- | ----------------------------------------------------- |
 | 1            | O(2^n\*n)       | O(n), recursive  | since n <= 20, naive dfs solution won't result in TLE |
+
+Update: 2024 / 07 / 03
+這題如果想成 moduler k 方式去計算 bucket，就可以變成完全互相獨立 select 相乘，不同 bucket
+不會有相差為 k 的問題，所以可以直接相乘。單一 bucket 選擇變成 robber 的 dp 問題，可選擇的種類
+完全取決於上一個最靠近他的 element 是否有被選擇
+
+| Solution Idx | Time Complexity | Space Complexity | Comment          |
+| ------------ | --------------- | ---------------- | ---------------- |
+| 2            | O(n)            | O(n)             | Hard solution .. |
--- Problems/2597/sol2.cpp
+++ Problems/2597/sol2.cpp
@@ -6,3 +6,46 @@
 #include <algorithm>
 
 using namespace std;
+
+class Solution
+{
+public:
+    int beautifulSubsets(vector<int> &nums, int k)
+    {
+        unordered_map<int, vector<pair<int, int>>> buckets;
+        unordered_map<int, int> count;
+        for (int n : nums)
+        {
+            count[n] += 1;
+        }
+        for (auto [val, cnt] : count)
+        {
+            buckets[val % k].push_back({val, cnt});
+        }
+        int res = 1;
+        for (auto it = buckets.begin(); it != buckets.end(); it++)
+        {
+            res *= robber((*it).second, k);
+        }
+        return res - 1;
+    }
+    int robber(vector<pair<int, int>> &bucket, int k)
+    {
+        sort(bucket.begin(), bucket.end());
+        int take = pow(2, bucket[0].second) - 1, notTake = 1;
+        for (int i = 1; i < bucket.size(); i++)
+        {
+            int oldTake = take;
+            if (bucket[i].first - k == bucket[i - 1].first)
+            {
+                take = notTake * (pow(2, bucket[i].second) - 1);
+            }
+            else
+            {
+                take = (take + notTake) * (pow(2, bucket[i].second) - 1);
+            }
+            notTake = oldTake + notTake;
+        }
+        return take + notTake;
+    }
+};
