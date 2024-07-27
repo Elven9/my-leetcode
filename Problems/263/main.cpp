aa02afff2d40297362c42cbf80757b0f272617ab
Elven Lin, RetrO9 <elvenlin1998@gmail.com>
2024-07-27 19:02:30 +0800

    cpp: 263 Ugly Number

---
+++ Problems/263/main.cpp
@@ -0,0 +1,35 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <map>
+#include <stack>
+#include <algorithm>
+#include <numeric>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
+#include <functional>
+
+using namespace std;
+
+class Solution
+{
+public:
+    bool isUgly(int n)
+    {
+        if (n == 0)
+            return false;
+        while (n != 1)
+        {
+            if (n % 2 == 0)
+                n /= 2;
+            else if (n % 3 == 0)
+                n /= 3;
+            else if (n % 5 == 0)
+                n /= 5;
+            else
+                return false;
+        }
+        return true;
+    }
+};
---
+++ Problems/263/readme.md
@@ -0,0 +1,5 @@
+# Ugly Nubmer
+
+| Solution Idx | Time Complexity | Space Complexity | Comment |
+| ------------ | --------------- | ---------------- | ------- |
+| 1            | O(logn)         | O(1)             |         |
