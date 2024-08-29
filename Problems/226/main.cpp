75592e62d359b31803fa6e66ccad91f0b71d1b6e
Elven Lin, RetrO9 <elvenlin1998@gmail.com>
2024-08-29 21:00:02 +0800

    update: 226

---
+++ Problems/226/main.cpp
@@ -0,0 +1,50 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <map>
+#include <stack>
+#include <algorithm>
+#include <numeric>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
+#include <functional>
+#include <sstream>
+#include <set>
+
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    TreeNode *invertTree(TreeNode *root)
+    {
+        queue<TreeNode *> q;
+        if (root)
+            q.push(root);
+
+        while (!q.empty())
+        {
+            auto n = q.front();
+            q.pop();
+
+            swap(n->left, n->right);
+            if (n->left)
+                q.push(n->left);
+            if (n->right)
+                q.push(n->right);
+        }
+
+        return root;
+    }
+};
