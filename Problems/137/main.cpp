ee70a80d3d60aa638ad62f48ed76901bdd4c0769
Elven Lin, RetrO9 <elvenlin1998@gmail.com>
2025-03-08 19:59:48 +0800

    cpp: 137 Single Number II

---
+++ Problems/137/main.cpp
@@ -0,0 +1,41 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <map>
+#include <stack>
+#include <algorithm>
+#include <numeric>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
+#include <functional>
+#include <sstream>
+#include <set>
+#include <cctype>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int singleNumber(vector<int> &nums)
+    {
+        vector<int> freq(32, 0);
+        for (auto &n : nums)
+        {
+            for (int i = 0; i < 32; i++)
+                freq[i] += (n >> i) & 1;
+        }
+
+        int res = 0;
+        for (int i = 0; i < 32; i++)
+        {
+            if (freq[i] % 3 != 0)
+                res |= (1 << i);
+        }
+
+        return res;
+    }
+};
+
+// constant space
---
+++ Problems/137/readme.md
@@ -0,0 +1,7 @@
+# Single Number II
+
+| Solution Idx | Time Complexity | Space Complexity | Comment             |
+| ------------ | --------------- | ---------------- | ------------------- |
+| 1            | O(n)            | O(1)             | Digits Manipulation |
+
+Digit doesn't have to be binary digit, it can be any format.
