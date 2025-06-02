d75cc9ca5f1dd71568c7b2a4b8407f43236f3bd7
Elven Lin, RetrO9 <elvenlin1998@gmail.com>
2025-06-02 10:59:04 +0800

    cpp: 3373 only TLE solution Q

---
+++ Problems/3373/TLE.cpp
@@ -0,0 +1,81 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <map>
+#include <stack>
+#include <algorithm>
+#include <numeric>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
+#include <functional>
+#include <sstream>
+#include <set>
+#include <cctype>
+
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> maxTargetNodes(vector<vector<int>> &edges1, vector<vector<int>> &edges2)
+    {
+        int n = edges1.size() + 1, m = edges2.size() + 1;
+        vector<vector<int>> t1(n);
+        vector<vector<int>> t2(m);
+
+        for (auto &e : edges1)
+        {
+            t1[e[0]].push_back(e[1]);
+            t1[e[1]].push_back(e[0]);
+        }
+        for (auto &e : edges2)
+        {
+            t2[e[0]].push_back(e[1]);
+            t2[e[1]].push_back(e[0]);
+        }
+
+        vector<int> res(n);
+        int max_t2 = INT_MIN;
+        for (int i = 0; i < m; i++)
+            max_t2 = max(max_t2, cal(t2, i, false));
+        for (int i = 0; i < n; i++)
+            res[i] = cal(t1, i, true) + max_t2;
+
+        return res;
+    }
+
+    int cal(vector<vector<int>> &tree, int st, bool isEven)
+    {
+        queue<int> q;
+        vector<bool> visited(1e5 + 1, false);
+        q.push(st);
+        visited[st] = true;
+
+        int res = 0, path = 0;
+        while (!q.empty())
+        {
+            int len = q.size();
+            if ((path % 2 == 0) == isEven)
+                res += len;
+            while (len-- > 0)
+            {
+                int t = q.front();
+                q.pop();
+
+                for (auto &c : tree[t])
+                {
+                    if (visited[c])
+                        continue;
+                    q.push(c);
+                    visited[c] = true;
+                }
+            }
+            path++;
+        }
+
+        return res;
+    }
+};
+
+// BFS on each node to find k lenght path (好險是 tree，不用用那些 shortest path)
---
+++ Problems/3373/readme.md
@@ -0,0 +1,5 @@
+# Maximize the Number of Target Nodes After Connecting Trees II
+
+| Solution Idx | Time Complexity | Space Complexity | Comment              |
+| ------------ | --------------- | ---------------- | -------------------- |
+| 1            | O(n^2)          | O(n^2)           | TLE, simple solution |
