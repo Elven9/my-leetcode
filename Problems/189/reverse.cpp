decf6a65e83039ada365adcc84101ae870810137
Elven Lin, RetrO9 <elvenlin1998@gmail.com>
2025-06-18 13:56:23 +0800

    update: 189

---
+++ Problems/189/reverse.cpp
@@ -0,0 +1,38 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <map>
+#include <stack>
+#include <algorithm>
+#include <numeric>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
+#include <functional>
+#include <sstream>
+#include <set>
+#include <cctype>
+
+using namespace std;
+
+class Solution
+{
+public:
+    void rotate(vector<int> &nums, int k)
+    {
+        int n = nums.size();
+        k = k % n;
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+    }
+    void reverse(vector<int> &nums, int st, int ed)
+    {
+        while (st < ed)
+        {
+            swap(nums[st], nums[ed]);
+            st++;
+            ed--;
+        }
+    }
+};
