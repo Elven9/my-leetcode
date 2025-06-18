4b443dc76f99b3ea02113cd39ce6d7cb0f6af9ce
Elven Lin, RetrO9 <elvenlin1998@gmail.com>
2025-06-18 16:27:27 +0800

    update: 759

--- Problems/759/main.cpp
+++ Problems/759/main.cpp
@@ -35,33 +35,31 @@ class Solution
 public:
     vector<Interval> employeeFreeTime(vector<vector<Interval>> schedule)
     {
-        vector<pair<int, int>> ts;
+        auto ts = vector<pair<int, int>>();
 
-        for (auto &w : schedule)
-            for (auto &i : w)
+        for (auto &e : schedule)
+            for (auto &i : e)
             {
+                // 小細節，這邊需要反過來，sort 才會把 start 排在 end 前面
                 ts.push_back({i.start, -1});
                 ts.push_back({i.end, 1});
             }
 
-        int n = ts.size();
         sort(ts.begin(), ts.end());
-
-        int idx = 0;
         vector<Interval> res;
-        while (idx < n)
+        int cur = 0, last = -1, st = -1;
+        for (auto &t : ts)
         {
-            int st, cnt = -ts[idx++].second;
-            while (idx < n && cnt != 0)
-            {
-                st = ts[idx].first;
-                cnt -= ts[idx++].second;
-            }
-            if (idx < n)
-            {
-                res.push_back(Interval(st, ts[idx].first));
-            }
+            cur -= t.second;
+            if (cur == 1 && last == 0)
+                res.push_back(Interval(st, t.first));
+            else if (cur == 0 && last == 1)
+                st = t.first;
+            last = cur;
         }
+
         return res;
     }
 };
+
+// 數量不多 覺得可以用 time stamp 處理比較快
