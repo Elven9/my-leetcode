d41e3b7c0363401dcca2f9b465cfcaab37359cd9
Elven Lin, RetrO9 <elvenlin1998@gmail.com>
2025-06-21 12:18:29 +0800

    update: 179

--- Problems/179/main.cpp
+++ Problems/179/main.cpp
@@ -16,21 +16,31 @@ using namespace std;
 class Solution
 {
 public:
-    string largestNumber(vector<int> &N)
+    string largestNumber(vector<int> &nums)
     {
-        vector<string> nums;
-        for (auto &n : N)
-            nums.push_back(to_string(n));
+        vector<string> list;
+        for (auto &n : nums)
+            list.push_back(to_string(n));
 
-        sort(nums.begin(), nums.end(), [](string a, string b) -> bool
-             { return (a + b) > (b + a); });
+        sort(list.rbegin(), list.rend(), [](string &a, string &b)
+             {
+            // 這真的好 greedy, 但一想卻很直觀 ..
+            // https://leetcode.com/problems/largest-number/solutions/53157/a-simple-c-solution
+            // https://github.com/wisdompeak/LeetCode/tree/master/Greedy/179.Largest-Number
+            return a+b < b+a; });
 
         stringstream ss;
-        for (auto &s : nums)
+        for (auto &s : list)
             ss << s;
+        string res = ss.str();
+        int i = 0;
+        while (i < res.size() - 1 && res[i] == '0')
+            i++;
 
-        if (ss.str()[0] == '0')
-            return "0";
-        return ss.str();
+        return res.substr(i);
     }
 };
+
+// convert to string
+//
+// sort reversely
