b349ff430ce516192eec4f3b5b77ef0d900ca3d4
Elven Lin, RetrO9 <elvenlin1998@gmail.com>
2025-06-21 16:56:04 +0800

    update: 73

--- Problems/73/main.cpp
+++ Problems/73/main.cpp
@@ -18,50 +18,47 @@ using namespace std;
 class Solution
 {
 public:
-    void setZeroes(vector<vector<int>> &M)
+    void setZeroes(vector<vector<int>> &matrix)
     {
-        int m = M.size(), n = M[0].size();
+        int m = matrix.size(), n = matrix[0].size();
+        // constant space, 不是叫你啥都不用 = =, 被限制住想法了
 
-        bool fc = false;
+        bool rowHasZero = false;
+        for (auto &n : matrix[0])
+            rowHasZero |= n == 0;
+
+        bool colHasZero = false;
         for (int i = 0; i < m; i++)
-            if (M[i][0] == 0)
-            {
-                fc = true;
-                break;
-            }
+            colHasZero |= matrix[i][0] == 0;
 
-        bool fr = false;
-        for (int i = 0; i < n; i++)
-            if (M[0][i] == 0)
-            {
-                fr = true;
-                break;
-            }
+        for (int j = 1; j < n; j++)
+            for (int i = 1; i < m; i++)
+                if (matrix[i][j] == 0)
+                {
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
 
+        // draw
         for (int i = 1; i < m; i++)
             for (int j = 1; j < n; j++)
-                if (M[i][j] == 0)
-                    M[i][0] = M[0][j] = 0;
+                if (matrix[0][j] == 0 || matrix[i][0] == 0)
+                    matrix[i][j] = 0;
 
-        for (int i = 1; i < m; i++)
-            for (int j = 1; j < n; j++)
-                if (M[i][0] == 0 || M[0][j] == 0)
-                    M[i][j] = 0;
+        if (rowHasZero)
+            for (int j = 0; j < n; j++)
+                matrix[0][j] = 0;
 
-        if (fc)
-        {
+        if (colHasZero)
             for (int i = 0; i < m; i++)
-                M[i][0] = 0;
-        }
-
-        if (fr)
-        {
-            for (int i = 0; i < n; i++)
-                M[0][i] = 0;
-        }
+                matrix[i][0] = 0;
     }
 };
 
-// will overlapped if updated first
-// no bit manipulation
-// no marker (since value range is from INT_MIN to INT_MAX)
+// 0 1 2 0
+// 3 4 5 2
+// 1 3 1 5
+
+// if (0, 0) == 0 -> 整個 row col 都要變 0
+// if (x, 0) has zero -> 整個 col 都要變 0
+// if (0, xx) has zero
